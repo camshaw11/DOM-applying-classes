85e3bf408fe5d913e74201b11c43a4ab212f1123
cameron <cameronshaw95@gmail.com>
2020-06-30 17:21:06 +1200

    add coloured dots to dom challenge

--- classes.js
+++ classes.js
@@ -1,25 +1,48 @@
 // Don't change or delete this line! It waits until the DOM has loaded, then calls 
 // the start function. More info: 
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
-document.addEventListener('DOMContentLoaded', start)
+document.addEventListener('DOMContentLoaded', start);
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+  one();
+  two();
+  three();
+  makeVisible();
   // Your turn! Create a new function called `two`, then call it from here.
-}
+} 
 
 function one () {
   // First, we have to find the element:
-  var one = document.getElementById('one')
+  var one = document.getElementById('one');
 
   // Next, we apply a new CSS class to it:
-  one.classList.add('blue')
+  one.classList.add('blue');
 }
 
 // CREATE FUNCTION two HERE
+function two() {
+  // grab element from html
+  let two = document.getElementById('two');
+
+  // add class green to element
+  two.classList.add('green');
+}
 
 // CREATE FUNCTION three HERE
+function three() {
+  // grab element from html
+  let three = document.getElementById('three');
+
+  // add class crimson to element
+  three.classList.add('crimson');
+}
 
 // CREATE FUNCTION makeVisible HERE
+function makeVisible() {
+  // grab element from html
+  let invisibleDiv = document.getElementsByClassName('invisible');
+
+  // add class to element
+  invisibleDiv[0].classList.add('visible');
+}
